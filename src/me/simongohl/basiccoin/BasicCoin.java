68748b44b55da7570be9ebab4ef780fef48b31c1
Simon Gohl <gohls@ITs-MacBook-Pro.local>
2021-02-11 22:57:27 -0500

    Initial

---
+++ src/me/simongohl/basiccoin/BasicCoin.java
@@ -0,0 +1,11 @@
+package me.simongohl.basiccoin;
+
+/*
+* Just Hello Basic-Coin for now 
+*/
+public class BasicCoin {
+	public static void main(String[] agrs) {
+		System.out.println("Hello Basic-Coin!");
+	}
+
+}
