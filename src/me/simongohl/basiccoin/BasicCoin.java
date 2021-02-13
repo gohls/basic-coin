858e87de12936eb2e46263c7761c749d65e18679
Simon Gohl <sagohl@gmail.com>
2021-02-13 18:22:39 -0500

    Comments and testing

--- src/me/simongohl/basiccoin/BasicCoin.java
+++ src/me/simongohl/basiccoin/BasicCoin.java
@@ -1,18 +1,42 @@
 package me.simongohl.basiccoin;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
-/*
-* I think I'm on the right track??
+
+/**
+* BasicCoin is a simple implementation of blockchain technology,
+* applying Proof-of-Work (PoW) as its consensus algorithm.
+*
+* Right now, it's just a terminal application. 
+* 
+* @author  Simon Gohl
+* @version 0.0.0
 */
 public class BasicCoin {
-	public static void main(String[] agrs) {
 	
-		ArrayList blockchain = new ArrayList<Block>();
-
-
+	public void blockchain() {
+		ArrayList<Block> blockchain = new ArrayList<Block>();
+		ArrayList<Transaction> pendingTransactions = new ArrayList<Transaction>();
+		int miningDifficulty = 2;
+		int blockSize = 5;
+		
+	 }
+	
+	public void addGenesisBlock() {};
+	
+	public void addTransaction() {};
+	
+	public void minePendingTransactions() {};
+	
+	
+	
+	public static void main(String[] agrs) throws NoSuchAlgorithmException {
+		System.out.println("Hello BasicCoin!");	
+		
+		Transaction transaction = new Transaction("me", "you", 510, "test");
+		System.out.println(transaction.hash);
+		
 	}
 
 }
