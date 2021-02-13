fdffa3c1e4514ea321a9cfb5a83a304b8198cc4b
Simon Gohl <sagohl@gmail.com>
2021-02-13 18:22:09 -0500

    Bugfix: fixed `loop` statement

--- src/me/simongohl/basiccoin/Utils.java
+++ src/me/simongohl/basiccoin/Utils.java
@@ -4,20 +4,23 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/*
+ * Source: 
+ * https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+ */
 public class Utils {
 	
 	private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 	
 	public static String calcHash(String str) throws NoSuchAlgorithmException {
 		final MessageDigest md = MessageDigest.getInstance("SHA-256");
-		final byte[] hashbytes = md.digest(str.getBytes(StandardCharsets.UTF_8));
-		final char[] hexChars = new char[hashbytes.length * 2];
-		for (byte b : hashbytes) {
-			int v = hashbytes[b] & 0xFF;
-			hexChars[b * 2] = HEX_ARRAY[v >>> 4];
-			hexChars[b * 2 + 1] = HEX_ARRAY[v & 0x0F];
-		}
-
+		final byte[] hashBytes = md.digest(str.getBytes(StandardCharsets.UTF_8));
+		final char[] hexChars = new char[hashBytes.length * 2];
+		for (int i = 0; i < hashBytes.length; i++) {
+	        int v = hashBytes[i] & 0xFF;
+	        hexChars[i * 2] = HEX_ARRAY[v >>> 4];
+	        hexChars[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
+	    }
 		return new String(hexChars);
 	}
 
